532689c7a1cd1620b454edfaec3b6c816a7e05de
rivkypaluch <rivkypaluch@gmail.com>
2025-08-07 12:31:25 +0300

    initiolize mini project - CI CD

---
+++ app.py
@@ -0,0 +1,12 @@
+# File: app.py
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, CI/CD World!'
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
+
